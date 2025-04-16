d57414e1d78a38125d9a475fa1d5915529840ff3
eivindtostesen <contact@tostesen.no>
2025-04-16 08:32:00 +0200

    add a peakoscope plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
+import matplotlib.pyplot as plt
 import streamlit as st
+import peakoscope
+import peakoscope.interface_matplotlib
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+
+X, Y = peakoscope.example_2()
+peaks = peakoscope.tree(Y)
+peakoscope.interface_matplotlib.TreeMatPlotLib(
+    peaks,
+    X=X,
+    Y=Y,
+    slice_of_x={n: n.subarray(X) for n in peaks},
+    slice_of_y={n: n.subarray(Y) for n in peaks},
 )
+fig, peaks.plot.ax = plt.subplots()
+peaks.plot.ax.plot(X, Y)
+peaks.plot.crowns(peaks.size_filter())
+peaks.plot.bounding_boxes(peaks.size_filter())
+
+st.pyplot(fig)
